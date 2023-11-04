719b602758c9456677a6bce0ac29ffd3ecca2671
Ryosuke Asano <ryosukeusuusu@gmail.com>
2023-11-04 13:06:48 +0900

    [FB] PWA | Make do not restore Window after Floorp restarts

--- browser/base/content/browser-ssb-support.js
+++ browser/base/content/browser-ssb-support.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -14,6 +15,13 @@ let gSsbSupport = {
           try {
             SiteSpecificBrowserIdUtils.runSSBWithId(Services.prefs.getStringPref("browser.ssb.startup"));
             Services.prefs.clearUserPref("browser.ssb.startup");
+            
+            // Add SSB Window or Tab Attribute
+            // This attribute is used to make do not restore the window or tab when the browser is restarted.
+            window.gBrowser.floorpSsbWindow = true;
+            gBrowser.tabs.forEach(tab => {
+              tab.setAttribute("floorpSSB", "true");
+            });    
           }
           catch(e) {
             console.log(e);
