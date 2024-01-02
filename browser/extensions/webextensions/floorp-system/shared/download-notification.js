0071f9ccc413692c5e08a5e4074086192b146984
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-01-02 20:54:43 +0900

    [FB] Donwload Notice | Make use of L10n

--- browser/extensions/webextensions/floorp-system/shared/download-notification.js
+++ browser/extensions/webextensions/floorp-system/shared/download-notification.js
@@ -5,27 +5,49 @@
 */
 const DOWNLOAD_NOTIFICATION_PREF = "floorp.download.notification";
 
-browser.downloads.onCreated.addListener(async(file) => {
-    let pref = String(await browser.aboutConfigPrefs.getPref(DOWNLOAD_NOTIFICATION_PREF));
-    if (pref === "1" || pref === "3") {
-        browser.notifications.create({
-          "type": "basic",
-          "iconUrl": "chrome://branding/content/about-logo.png",
-          "title": chrome.i18n.getMessage("start"),
-          "message": file.filename
-        });
-    }
+browser.downloads.onCreated.addListener(async file => {
+  let getL10nData = await browser.browserL10n.getFloorpL10nValues({
+    file: ["browser/floorp.ftl"],
+    text: ["floorp-started-download"],
+  });
+  let localizedList = [];
+  for (let key in getL10nData) {
+    localizedList.push(getL10nData[key]);
+  }
+
+  let pref = String(
+    await browser.aboutConfigPrefs.getPref(DOWNLOAD_NOTIFICATION_PREF)
+  );
+  if (pref === "1" || pref === "3") {
+    browser.notifications.create({
+      type: "basic",
+      iconUrl: "chrome://branding/content/about-logo.png",
+      title: localizedList[0],
+      message: file.filename,
+    });
+  }
 });
 
-browser.downloads.onChanged.addListener(async(file) => {
-      let pref = String(await browser.aboutConfigPrefs.getPref(DOWNLOAD_NOTIFICATION_PREF));
-      if (file.state.current == "complete" && (pref === "2" || pref === "3")) {
-        let download = await browser.downloads.search({ id: file.id });
-        browser.notifications.create({
-          "type": "basic",
-          "iconUrl": "chrome://branding/content/about-logo.png",
-          "title": chrome.i18n.getMessage("finish"),
-          "message": download[0].filename
-        });
-    }
+browser.downloads.onChanged.addListener(async file => {
+  let getL10nData = await browser.browserL10n.getFloorpL10nValues({
+    file: ["browser/floorp.ftl"],
+    text: ["floorp-finished-download"],
+  });
+  let localizedList = [];
+  for (let key in getL10nData) {
+    localizedList.push(getL10nData[key]);
+  }
+
+  let pref = String(
+    await browser.aboutConfigPrefs.getPref(DOWNLOAD_NOTIFICATION_PREF)
+  );
+  if (file.state.current == "complete" && (pref === "2" || pref === "3")) {
+    let download = await browser.downloads.search({ id: file.id });
+    browser.notifications.create({
+      type: "basic",
+      iconUrl: "chrome://branding/content/about-logo.png",
+      title: localizedList[0],
+      message: download[0].filename,
+    });
+  }
 });
