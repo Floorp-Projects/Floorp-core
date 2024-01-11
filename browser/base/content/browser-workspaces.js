e0b1c0f5177511f54d0065518a4dce8e0c07d36d
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-01-11 19:21:46 +0900

    [FB] Workspaces | Use L10n

--- browser/base/content/browser-workspaces.js
+++ browser/base/content/browser-workspaces.js
@@ -421,7 +421,7 @@ var gWorkspaces = {
   },
 
   async createNoNameWorkspace() {
-    await this.createWorkspace("New Workspace", false, true);
+    await this.createWorkspace(this.l10n.formatValueSync("workspace-default-name"), false, true);
   },
 
   async deleteWorkspace(workspaceId) {
