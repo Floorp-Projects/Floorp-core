6b79feecd3920dc3843f2e1581f2699ffefa30b8
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-02-23 22:18:00 +0900

    [FB] Extensions | Fix #942

--- browser/app/profile/000-floorp.js
+++ browser/app/profile/000-floorp.js
@@ -105,8 +105,6 @@ pref("floorp.browser.note.memos.using", -1);
 pref("services.sync.prefs.sync.floorp.browser.note.memos", true);
 pref("floorp.browser.note.enabled", false);
 
-//Clock
-pref("floorp.browser.clock.enabled", false);
 
 //新規タブの開く位置
 pref("floorp.browser.tabs.openNewTabPosition", -1);
@@ -172,6 +170,9 @@ pref("floorp.browser.workspaces.enabled", true);
 pref("floorp.browser.workspace.manageOnBMS", false);
 pref("floorp.browser.workspace.closePopupAfterClick", false);
 pref("floorp.browser.workspace.showWorkspaceName", true);
+
+// Extension
+pref("floorp.extensions.allowPrivateBrowsingByDefault.is.enabled", false);
 
 /*----------------------------------------------------------------------------------------------------------------------------------*/
 
