2f1bf579e02e7da3af7b83b37141cd43e87fd574
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-04-29 23:45:45 +0900

    [FB] ESM | Migrate browser actions JS to ESM

--- browser/base/content/browser-browserActions.js
+++ browser/base/content/browser-browserActions.js
@@ -4,171 +4,125 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const { CustomizableUI } = ChromeUtils.importESModule(
+var { CustomizableUI } = ChromeUtils.importESModule(
   "resource:///modules/CustomizableUI.sys.mjs"
 );
 
-async function UCTFirst() {
-  const widgetId = "undo-closed-tab";
-  const widget = CustomizableUI.getWidget(widgetId);
-  if (widget && widget.type != "custom") {
-    return;
-  }
-  const l10n = new Localization(["browser/floorp.ftl"]);
-  const l10nText =
-    (await l10n.formatValue("undo-closed-tab")) ?? "Undo close tab";
-  CustomizableUI.createWidget({
-    id: widgetId,
-    type: "button",
-    label: l10nText,
-    tooltiptext: l10nText,
-    onCreated(aNode) {
-      const fragment = window.MozXULElement.parseXULToFragment(
-        `<stack xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" class="toolbarbutton-badge-stack"><image class="toolbarbutton-icon" label="閉じたタブを開く"/><html:label xmlns:html="http://www.w3.org/1999/xhtml" class="toolbarbutton-badge" ></html:label></stack>`
-      );
-      aNode.appendChild(fragment);
-    },
-    onCommand() {
-      undoCloseTab();
-    },
-  });
-  if (
-    ChromeUtils.importESModule("resource:///modules/FloorpStartup.sys.mjs")
-      .isFirstRun
-  ) {
-    CustomizableUI.addWidgetToArea(widgetId, CustomizableUI.AREA_NAVBAR, -1);
-  }
-}
+/**
+ * Object representing the browser actions for the Floorp features.
+ * If you search Workspaces's toolbar button, You can find it below path.
+ * path: Floorp-private-components/browser/base/content/browser-workspaces.mjs
+ *
+ * @type {object}
+ * @property {boolean} _initialized - Indicates whether the browser actions have been initialized.
+ * @property {Function} init - Initializes the browser actions.
+ * @property {Function} createUndoCloseTabButton - Creates the undo close tab button.
+ * @property {Function} createSwitchSidebarPositionButton - Creates the switch sidebar position button.
+ * @property {Function} createProfileManagerButton - Creates the profile manager button.
+ */
+const gFloorpBrowserActions = {
+  _initialized: false,
 
-UCTFirst();
+  init() {
+    if (this._initialized) {
+      return;
+    }
 
-async function switchSidebarPositionButton() {
-  const widgetId = "sidebar-reverse-position-toolbar";
-  const widget = CustomizableUI.getWidget(widgetId);
-  if (widget && widget.type !== "custom") {
-    return;
-  }
-  const l10n = new Localization(["browser/floorp.ftl"]);
-  const l10nText = await l10n.formatValue("sidebar-reverse-position-toolbar");
-  CustomizableUI.createWidget({
-    id: widgetId,
-    type: "button",
-    label: l10nText,
-    tooltiptext: l10nText,
-    onCreated(aNode) {
-      const fragment = window.MozXULElement.parseXULToFragment(
-        `<stack xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" class="toolbarbutton-badge-stack"><image class="toolbarbutton-icon" label="閉じたタブを開く"/><html:label xmlns:html="http://www.w3.org/1999/xhtml" class="toolbarbutton-badge" ></html:label></stack>`
-      );
-      aNode.appendChild(fragment);
-    },
-    onCommand() {
-      SidebarUI.reversePosition();
-    },
-  });
-  if (
-    ChromeUtils.importESModule("resource:///modules/FloorpStartup.sys.mjs")
-      .isFirstRun
-  ) {
-    CustomizableUI.addWidgetToArea(
-      "sidebar-button",
-      CustomizableUI.AREA_NAVBAR,
-      3
+    this.createUndoCloseTabButton();
+    this.createSwitchSidebarPositionButton();
+    this._initialized = true;
+  },
+
+  async _createToolbarButton(widgetId, l10nId, onCommand, type = "button") {
+    const widget = CustomizableUI.getWidget(widgetId);
+    if (widget && widget.type !== "custom") {
+      return;
+    }
+    const l10n = new Localization(["browser/floorp.ftl"]);
+    const l10nText = await l10n.formatValue(l10nId);
+    CustomizableUI.createWidget({
+      id: widgetId,
+      type,
+      label: l10nText,
+      tooltiptext: l10nText,
+      onCommand() {
+        Function(onCommand)();
+      },
+      onCreated(aNode) {
+        const fragment = window.MozXULElement.parseXULToFragment(
+          `<stack xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul" class="toolbarbutton-badge-stack">
+            <image class="toolbarbutton-icon" data-l10n-id="${l10nId}" />
+            <html:label xmlns:html="http://www.w3.org/1999/xhtml" class="toolbarbutton-badge" />
+           </stack>`
+        );
+        aNode.appendChild(fragment);
+      },
+    });
+  },
+
+  async createUndoCloseTabButton() {
+    await this._createToolbarButton(
+      "undo-closed-tab",
+      "undo-closed-tab",
+      "undoCloseTab();",
+      "button"
+    ).then(() => {
+      if (
+        ChromeUtils.importESModule("resource:///modules/FloorpStartup.sys.mjs")
+          .isFirstRun
+      ) {
+        CustomizableUI.addWidgetToArea(
+          widgetId,
+          CustomizableUI.AREA_NAVBAR,
+          -1
+        );
+      }
+    });
+  },
+
+  async createSwitchSidebarPositionButton() {
+    await this._createToolbarButton(
+      "sidebar-reverse-position-toolbar",
+      "sidebar-reverse-position-toolbar",
+      "SidebarUI.reversePosition();",
+      "button"
+    ).then(() => {
+      if (
+        ChromeUtils.importESModule("resource:///modules/FloorpStartup.sys.mjs")
+          .isFirstRun
+      ) {
+        CustomizableUI.addWidgetToArea(
+          "sidebar-button",
+          CustomizableUI.AREA_NAVBAR,
+          3
+        );
+        CustomizableUI.addWidgetToArea(widgetId, CustomizableUI.AREA_NAVBAR, 4);
+      }
+    });
+  },
+
+  async createProfileManagerButton() {
+    if (!Services.prefs.getBoolPref("floorp.browser.profile-manager.enabled")) {
+      return;
+    }
+
+    await this._createToolbarButton(
+      "profile-manager",
+      "floorp-profile-manager",
+      `(async () => {
+        const popup = document.getElementById("profile-manager-popup");
+        popup.openPopup(
+          document.getElementById("profile-manager-popup"),
+          "after_start",
+          0,
+          0,
+          false,
+          false
+        );
+      })();`,
+      "button"
     );
-    CustomizableUI.addWidgetToArea(widgetId, CustomizableUI.AREA_NAVBAR, 4);
-  }
-}
+  },
+};
 
-switchSidebarPositionButton();
-
-async function profileManager() {
-  const widgetId = "profile-manager";
-  const widget = CustomizableUI.getWidget(widgetId);
-  if (widget && widget.type !== "custom") {
-    return;
-  }
-  const l10n = new Localization(["browser/floorp.ftl"]);
-  const l10nText = await l10n.formatValue("floorp-profile-manager");
-  CustomizableUI.createWidget({
-    id: widgetId,
-    type: "button",
-    label: l10nText,
-    tooltiptext: l10nText,
-    onCreated(aNode) {
-      aNode.setAttribute("type", "menu");
-      const popup = window.MozXULElement.parseXULToFragment(`
-        <menupopup id="profile-manager-popup" position="after_start" style="--panel-padding: 0 !important;">
-          <browser id="profile-switcher-browser" src="chrome://browser/content/profile-manager/profile-switcher.xhtml" flex="1" type="content" disablehistory="true" disableglobalhistory="true" context="profile-popup-contextmenu" />
-        </menupopup>
-      `);
-      aNode.style = "--panel-padding: 0 !important;";
-      aNode.appendChild(popup);
-    },
-    onCommand() {
-      const popup = document.getElementById("profile-manager-popup");
-      popup.openPopup(
-        document.getElementById("profile-manager-popup"),
-        "after_start",
-        0,
-        0,
-        false,
-        false
-      );
-    },
-  });
-}
-
-if (
-  Services.prefs.getBoolPref("floorp.browser.profile-manager.enabled", false)
-) {
-  profileManager();
-}
-
-async function workspacesToolbarButton() {
-  let { WorkspacesElementService } = ChromeUtils.importESModule(
-    "resource:///modules/WorkspacesElementService.sys.mjs"
-  );
-
-  const widgetId = "workspaces-toolbar-button";
-  const widget = CustomizableUI.getWidget(widgetId);
-  if (widget && widget.type !== "custom") {
-    return;
-  }
-  const l10n = new Localization(["browser/floorp.ftl", "branding/brand.ftl"]);
-  const l10nText = await l10n.formatValue("workspaces-toolbar-button");
-  CustomizableUI.createWidget({
-    id: widgetId,
-    type: "button",
-    label: l10nText,
-    tooltiptext: l10nText,
-    overflows: false,
-    removable: true,
-    onCreated(aNode) {
-      aNode.setAttribute("type", "menu");
-      const popup = window.MozXULElement.parseXULToFragment(
-        WorkspacesElementService.panelElement
-      );
-      aNode.appendChild(popup);
-    },
-    onCommand() {
-      let currentWindow = Services.wm.getMostRecentWindow("navigator:browser");
-      const panel = currentWindow.document.getElementById(
-        "workspacesToolbarButtonPanel"
-      );
-      panel.openPopup(
-        document.getElementById("workspaces-toolbar-button"),
-        "bottomright topright",
-        0,
-        0,
-        false,
-        false
-      );
-    },
-  });
-  if (
-    ChromeUtils.importESModule("resource:///modules/FloorpStartup.sys.mjs")
-      .isFirstRun
-  ) {
-    CustomizableUI.addWidgetToArea(widgetId, CustomizableUI.AREA_TABSTRIP);
-    CustomizableUI.moveWidgetWithinArea(widgetId, -1);
-  }
-}
+gFloorpBrowserActions.init();
