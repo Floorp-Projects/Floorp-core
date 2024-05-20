d6cce3cecdeff721ccf02b34037ffbc5fb065811
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-05-20 23:27:18 +0900

    [FB] CSK | add MPL2.0 license note

--- nora/src/preferences/csk/category.tsx
+++ nora/src/preferences/csk/category.tsx
@@ -1,4 +1,7 @@
-import { onMount } from "solid-js";
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import type {} from "@solid-xul/jsx-runtime";
 
 export function category() {
