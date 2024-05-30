8439205c00530709de7243bcc15df82711622e07
Ryosuke Asano <ryosukeusuusu@gmail.com>
2024-05-30 23:30:23 +0900

    [FB] Nora | Use mach build instead of mach build faster

--- nora/vite.config.ts
+++ nora/vite.config.ts
@@ -5,6 +5,7 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { $ } from "execa";
+import { promises as fs } from "node:fs";
 
 import * as path from "node:path";
 import { generateJarManifest } from "./scripts/gen_jarmn";
@@ -82,18 +83,22 @@ export default defineConfig({
               }
               const _cwd = process.cwd();
               process.chdir("../..");
-              await $({ stdio: "inherit" })`./mach build faster`;
+              await $({ stdio: "inherit" })`./mach build`;
               process.chdir(_cwd);
 
+              //https://github.com/puppeteer/puppeteer/blob/c229fc8f9750a4c87d0ed3c7b541c31c8da5eaab/packages/puppeteer-core/src/node/FirefoxLauncher.ts#L123
+              await fs.mkdir("./dist/profile/test", { recursive: true });
               browser = await puppeteer.launch({
                 headless: false,
-                protocol: "webDriverBiDi",
+                cdp: "webDriverBiDi",
                 dumpio: true,
                 product: "firefox",
                 executablePath: path.join("../..", binPath("windows-x64")),
                 userDataDir: "./dist/profile/test",
                 extraPrefsFirefox: { "browser.newtabpage.enabled": true },
+                defaultViewport: { height: 0, width: 0 },
               });
+              (await browser.pages())[0].setViewport({ width: 0, height: 0 });
               (await browser.pages())[0].goto("about:newtab");
               browser.on("disconnected", () => {
                 if (!intended_close) process.exit(1);
