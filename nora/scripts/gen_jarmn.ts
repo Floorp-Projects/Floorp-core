774194adba793519a05caf64ff4fcdce4febcd91
NyanRus <68762426+nyanrus@users.noreply.github.com>
2024-05-30 22:52:03 +0900

    [FB] Build | add puppeteer to launch browser

--- nora/scripts/gen_jarmn.ts
+++ nora/scripts/gen_jarmn.ts
@@ -12,14 +12,11 @@ export async function generateJarManifest(bundle: object) {
   for (const i of Object.values(viteManifest)) {
     arr.push((i as { fileName: string })["fileName"]);
   }
-
-  await fs.writeFile(
-    "dist/jar.mn",
-    `floorp.jar:\n% content nora %nora/ contentaccessible=yes\n ${Array.from(
-      new Set(arr),
-    )
-      .map((v) => `nora/${v} (${v})`)
-      .join("\n ")}`,
-  );
   console.log("generate end jar.mn");
+
+  return `floorp.jar:\n% content nora %nora/ contentaccessible=yes\n ${Array.from(
+    new Set(arr),
+  )
+    .map((v) => `nora/${v} (${v})`)
+    .join("\n ")}`;
 }
--- nora/vite.config.ts
+++ nora/vite.config.ts
@@ -4,13 +4,24 @@
 
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { $ } from "execa";
 
 import * as path from "node:path";
 import { generateJarManifest } from "./scripts/gen_jarmn";
+import puppeteer from "puppeteer-core";
+import { Browser } from "puppeteer-core";
 
 const r = (dir: string) => {
   return path.resolve(import.meta.dirname, dir);
 };
+
+function binPath(platform: "windows-x64"): string {
+  switch (platform) {
+    case "windows-x64": {
+      return "obj-x86_64-pc-windows-msvc/dist/bin/floorp.exe";
+    }
+  }
+}
 
 export default defineConfig({
   publicDir: r("public"),
@@ -44,10 +55,54 @@ export default defineConfig({
     }),
     {
       name: "gen_jarmn",
-      generateBundle(options, bundle, isWrite) {
-        generateJarManifest(bundle);
+      enforce: "post",
+      async generateBundle(options, bundle, isWrite) {
+        this.emitFile({
+          type: "asset",
+          fileName: "jar.mn",
+          needsCodeReference: false,
+          source: await generateJarManifest(bundle),
+        });
       },
     },
+    (() => {
+      let browser: Browser;
+      let intended_close = false;
+      return {
+        name: "run_browser",
+        enforce: "post",
+        buildEnd(error) {
+          (async () => {
+            if (this.meta.watchMode) {
+              if (browser) {
+                console.log("Browser Restarting...");
+                intended_close = true;
+                await browser.close();
+                intended_close = false;
+              }
+              const _cwd = process.cwd();
+              process.chdir("../..");
+              await $({ stdio: "inherit" })`./mach build faster`;
+              process.chdir(_cwd);
+
+              browser = await puppeteer.launch({
+                headless: false,
+                protocol: "webDriverBiDi",
+                dumpio: true,
+                product: "firefox",
+                executablePath: binPath("windows-x64"),
+                userDataDir: "./dist/profile/test",
+                extraPrefsFirefox: { "browser.newtabpage.enabled": true },
+              });
+              (await browser.pages())[0].goto("about:newtab");
+              browser.on("disconnected", () => {
+                if (!intended_close) process.exit(1);
+              });
+            }
+          })();
+        },
+      };
+    })(),
   ],
   resolve: {
     alias: {
