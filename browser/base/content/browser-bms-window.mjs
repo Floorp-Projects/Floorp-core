d8402ab24402cfc3f3ebbdd81de11cb19ff09c5a
regularentropy <roottotalhax@gmail.com>
2024-08-15 14:25:39 +0200

    [FB] BMSWindow | Clean up

--- browser/base/content/browser-bms-window.mjs
+++ browser/base/content/browser-bms-window.mjs
@@ -27,94 +27,79 @@ export var gBmsWindow = {
   },
 
   get webpanelData() {
-    let id = this.webpanelId;
-    let data = BrowserManagerSidebarPanelWindowUtils.BROWSER_SIDEBAR_DATA.data;
-    return data[id];
+    const id = this.webpanelId;
+    return BrowserManagerSidebarPanelWindowUtils.BROWSER_SIDEBAR_DATA.data[id];
   },
 
   get isBmsWindow() {
-    return !!this.currentURL.searchParams.get("floorpWebPanelId");
+    return Boolean(this.webpanelId);
   },
 
   init() {
-    if (this._initialized) {
-      return;
-    }
-
-    let webPanelId = new URL(window.location.href).searchParams.get(
-      "floorpWebPanelId"
-    );
-    if (!webPanelId) {
-      return;
-    }
+    if (this._initialized || !this.webpanelId) return;
 
     // Against session restore issue
     window.floorpWebPanelWindow = true;
-    window.SessionStore.promiseInitialized.then(() => {
-      this.createWebpanelWindow();
-    });
+    window.SessionStore.promiseInitialized.then(() => this.createWebpanelWindow());
 
-    // finish initialization
+    // Finish initialization
     this._initialized = true;
   },
 
   createWebpanelWindow() {
-    let webPanelId = this.currentURL.searchParams.get("floorpWebPanelId");
-    let userContextId = this.userContextId;
-    let userAgent = this.userAgent;
-    let loadURL = this.loadURL;
+    const { userContextId, userAgent, loadURL, mainWindow } = this;
 
-    this.mainWindow.setAttribute("BSM-window", "true");
-    this.mainWindow.setAttribute("BMS-usercontextid", userContextId);
-    this.mainWindow.setAttribute("BMS-useragent", userAgent);
-    this.mainWindow.setAttribute("BMS-webpanelid", webPanelId);
+    mainWindow.setAttribute("BSM-window", "true");
+    mainWindow.setAttribute("BMS-usercontextid", userContextId);
+    mainWindow.setAttribute("BMS-useragent", userAgent);
+    mainWindow.setAttribute("BMS-webpanelid", this.webpanelId);
     window.bmsLoadedURI = loadURL;
 
     // Remove "navigator:browser" from window-main attribute
-    this.mainWindow.setAttribute("windowtype", "navigator:webpanel");
+    mainWindow.setAttribute("windowtype", "navigator:webpanel");
 
     // Tab modifications
     window.gBrowser.loadURI(Services.io.newURI(loadURL), {
       triggeringPrincipal: Services.scriptSecurityManager.getSystemPrincipal(),
     });
 
-    // Atribute modifications
+    // Attribute modifications
     window.gBrowser.selectedTab.setAttribute("BMS-webpanel-tab", "true");
 
-    // userContextId
-    if (userContextId != 0) {
+    // User context handling
+    if (userContextId !== 0) {
       window.setTimeout(() => {
         BrowserManagerSidebarPanelWindowUtils.reopenInSelectContainer(
-          window,
-          webPanelId,
-          userContextId,
-          false
+            window,
+            this.webpanelId,
+            userContextId,
+            false
         );
       }, 0);
     }
 
-    // Resolve the issue that open url by anoter application and that is not loaded on main window.
+    // Handle issue of opening URL by another application and not loading on the main window
     window.setTimeout(() => {
-      let tab = window.gBrowser.addTrustedTab("about:blank");
+      const tab = window.gBrowser.addTrustedTab("about:blank");
       window.gBrowser.removeTab(tab);
     }, 0);
 
-    // Windows moficiations
-    this.mainWindow.setAttribute(
-      "chromehidden",
-      "toolbar",
-      "menubar directories extrachrome chrome,location=yes,centerscreen,dialog=no,resizable=yes,scrollbars=yes"
+    // Window modifications
+    mainWindow.setAttribute(
+        "chromehidden",
+        "toolbar menubar directories extrachrome",
+        "chrome,location=yes,centerscreen,dialog=no,resizable=yes,scrollbars=yes"
     );
 
-    const BMSSyleElement = document.createElement("style");
-    BMSSyleElement.textContent = `
-           @import url("chrome://floorp/content/browser-bms-window.css");
-        `;
-    document.head.appendChild(BMSSyleElement);
+    const BMSStyleElement = document.createElement("style");
+    BMSStyleElement.textContent = `
+      @import url("chrome://floorp/content/browser-bms-window.css");
+    `;
+    document.head.appendChild(BMSStyleElement);
 
-    let setZoomLebelInterval = window.setInterval(() => {
+    const setZoomLevelInterval = window.setInterval(() => {
       if (window.closed) {
-        window.clearInterval(setZoomLebelInterval);
+        window.clearInterval(setZoomLevelInterval);
         return;
       }
 
@@ -123,7 +108,7 @@ export var gBmsWindow = {
   },
 
   setZoomLevel() {
-    let zoomLevel = gBmsWindow.webpanelData.zoomLevel;
+    const zoomLevel = this.webpanelData?.zoomLevel;
     if (zoomLevel) {
       window.ZoomManager.zoom = zoomLevel;
     }
