eecd4f457a214f0a28b6c17285635794320e5d02
Ryosuke Asano <s1F102400011@iniad.org>
2024-08-17 14:44:53 +0900

    [FB] SplyView | BUg fix

--- browser/base/content/browser-splitView.mjs
+++ browser/base/content/browser-splitView.mjs
@@ -322,9 +322,9 @@ export class SplitView {
       ];
     if (!result) {
       result =
-      window.gBrowser.tabs[
-        window.gBrowser.tabs.indexOf(window.gBrowser.selectedTab) + 1
-      ];
+        window.gBrowser.tabs[
+          window.gBrowser.tabs.indexOf(window.gBrowser.selectedTab) + 1
+        ];
     }
 
     return result ?? null;
@@ -408,7 +408,7 @@ export class SplitView {
   /**
    * Deactivates the split view.
    */
-  deactivateSplitView() 
+  deactivateSplitView() {
     for (const tab of this._data[this.currentView].tabs) {
       const container = tab.linkedBrowser.closest(".browserSidebarContainer");
       this.resetContainerStyle(container);
@@ -420,6 +420,7 @@ export class SplitView {
     this.setTabsDocShellState(this._data[this.currentView].tabs, false);
     this.currentView = -1;
     this.hideSplitViewManager();
+  }
 
   /**
    * Activates the split view.
@@ -460,7 +461,7 @@ export class SplitView {
     activeTab,
     reverse = true,
     method = "column"
-  ) 
+  ) {
     this.tabBrowserPanel.style.flexDirection = this.getFlexDirection(
       reverse,
       method
@@ -477,12 +478,14 @@ export class SplitView {
         reverse
       );
     });
+  }
 
-  getFlexDirection(reverse, method) 
+  getFlexDirection(reverse, method) {
     if (method === "column") {
       return reverse ? "column-reverse" : "column";
     }
     return reverse ? "row-reverse" : "row";
+  }
 
   /**
    * Styles the container for a tab.
@@ -492,7 +495,7 @@ export class SplitView {
    * @param {number} index - The index of the tab.
    * @param {string} flexType - The type of flex layout.
    */
-  styleContainer(container, isActive, index, flexType) 
+  styleContainer(container, isActive, index, flexType) {
     container.removeAttribute("split-active");
     if (isActive) {
       container.setAttribute("split-active", "true");
@@ -504,6 +507,7 @@ export class SplitView {
       container.style.flex = "1";
       container.style.order = index;
     }
+  }
 
   /**
    * Handles the tab click event.
@@ -526,7 +530,7 @@ export class SplitView {
    * @param {Tab[]} tabs - The tabs.
    * @param {boolean} active - Indicates if the tabs are active.
    */
-  setTabsDocShellState(tabs, active) 
+  setTabsDocShellState(tabs, active) {
     for (const tab of tabs) {
       tab.linkedBrowser.spliting = active;
       tab.linkedBrowser.docShellIsActive = active;
@@ -545,24 +549,26 @@ export class SplitView {
         browser.removeAttribute("style");
       }
     }
+  }
 
   /**
    * Resets the container style.
    *
    * @param {Element} container - The container element.
    */
-  resetContainerStyle(container) 
+  resetContainerStyle(container) {
     container.removeAttribute("split-active");
     container.classList.remove("deck-selected");
     container.style.flex = "";
     container.style.order = "";
+  }
 
   /**
    * Gets the modifier element.
    *
    * @returns {Element} The modifier element.
    */
-  get modifierElement() 
+  get modifierElement() {
     if (!this.__modifierElement) {
       const wrapper = document.getElementById("template-split-view-modifier");
       const panel = wrapper.content.firstElementChild;
@@ -570,6 +576,7 @@ export class SplitView {
       this.__modifierElement = panel;
     }
     return this.__modifierElement;
+  }
 
   /**
    * @description unsplit the current view.]
