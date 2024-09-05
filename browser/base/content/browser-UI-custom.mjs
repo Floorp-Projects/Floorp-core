0d65d9040bf11c84d4de95af0759f75c72cf6eec
Ryosuke Asano <s1F102400011@iniad.org>
2024-09-05 16:31:41 +0900

    [FB] Navbar bottom | Fix cannot revert to default

--- browser/base/content/browser-UI-custom.mjs
+++ browser/base/content/browser-UI-custom.mjs
@@ -186,7 +186,7 @@ gFloorpObservePreference("floorp.navbar.bottom", function (event) {
         .appendChild(document.getElementById("nav-bar"));
       document
         .querySelector(".urlbarView")
-        .after(document.querySelector(".urlbar-input-container"));
+        .before(document.querySelector(".urlbar-input-container"));
       if (
         !Services.prefs.getBoolPref("floorp.bookmarks.fakestatus.mode", false)
       ) {
